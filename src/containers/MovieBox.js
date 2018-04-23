de4befe8c991111b6b610dbdfba1bc91c2088f6c
Christiebelle <cslester@outlook.com>
2018-04-23 17:15:07 +0100

    minor refactor to moviebox, changing movie to title

--- src/containers/MovieBox.js
+++ src/containers/MovieBox.js
@@ -7,11 +7,11 @@ class MovieBox extends Component{
   constructor(props){
     super(props);
     this.state = {
-      data:[{id: 1, movie: "Chariots of Fire", showtimes: "1600, 1730, 2000, 2100, 2230"},
-            {id: 2, movie: "Singin' In The Rain", showtimes: "1530, 1715, 2015, 2130, 2200"},
-            {id: 3, movie: "Gone With The Wind", showtimes: "1500, 1730, 1900, 2015, 2145"},
-            {id: 4, movie: "Calamity Jane", showtimes: "1600, 1730, 1845, 1930, 2200"},
-            {id: 5, movie: "Casablanca", showtimes: "1600, 1730, 1915, 2145, 2215"}]
+      data:[{id: 1, title: "Chariots of Fire", showtimes: "1600, 1730, 2000, 2100, 2230"},
+            {id: 2, title: "Singin' In The Rain", showtimes: "1530, 1715, 2015, 2130, 2200"},
+            {id: 3, title: "Gone With The Wind", showtimes: "1500, 1730, 1900, 2015, 2145"},
+            {id: 4, title: "Calamity Jane", showtimes: "1600, 1730, 1845, 1930, 2200"},
+            {id: 5, title: "Casablanca", showtimes: "1600, 1730, 1915, 2145, 2215"}]
     };
   }
 
