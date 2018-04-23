7a40efa5c43f12704da1b92ebffefc7123d771e5
Christiebelle <cslester@outlook.com>
2018-04-23 17:14:22 +0100

    movielist file created

---
+++ src/components/MovieList.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import Movie from "./Movie";
+
+class MovieList extends Component{
+  render(){
+    const movieNodes = this.props.data.map(Movie => {
+      return(
+        <Movie author={Movie.title} key={Movie.id}>
+          {Movie.showtimes}
+        </Movie>
+      );
+    });
+    return <div className="movielist">{movietNodes}</div>
+  }
+}
+
+
+
+export default MovieList;
